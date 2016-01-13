e4f521c58676744917a43e0c8b64f2725dbf5355
Duffman100 <duffy.craig@gmail.com>
2016-01-13 21:30:23 +0000

    initial load

---
+++ Blocs/Array2D.swift
@@ -0,0 +1,31 @@
+//
+//  Array2D.swift
+//  Blocs
+//
+//  Created by Craig Duffy on 20/01/2015.
+//  Copyright (c) 2015 Craig Duffy. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    // #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        // #3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    // #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
